4db84f23145e886de0311b1307f63232d2ffd32c
faiq <mfaiqsyahmi9@gmail.com>
2026-05-27 23:57:19 +0800

    config: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        tailwindcss(),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    const host = env.VITE_DEV_SERVER_HOST || '0.0.0.0';
+    const port = env.VITE_DEV_SERVER_PORT || 5173;
+
+    return {
+        plugins: [
+            tailwindcss(),
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+        server: {
+            host: '0.0.0.0',
+            https: false,
+            origin: `http://${host}:${port}`,
+            hmr: {
+                host: host,
+            },
+        },
+    };
 });
